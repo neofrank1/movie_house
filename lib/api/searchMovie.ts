02346a033c18cb1c28a454f5de780fec2b5150f2
neofrankuy21 <neofrankuy21@gmail.com>
2025-11-27 22:20:09 +0800

    Fix the Error Axios

--- lib/api/searchMovie.ts
+++ lib/api/searchMovie.ts
@@ -5,7 +5,7 @@ export default async function searchMovie(searchMovieQuery: searchMovieQuery) {
   try {
     const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY || "";
     const apiUrl = process.env.NEXT_PUBLIC_OMDB_API_URL || "http://www.omdbapi.com";
-    const response = await axios.get(`${apiUrl}/?apikey=${apiKey}&`, {
+    const response = await axios.get(`${apiUrl}/?apikey=${apiKey}`, {
       params: searchMovieQuery,
       headers: {
         "Content-Type": "application/json",
@@ -23,7 +23,7 @@ export async function getMovieDetails(movieDetailsQuery: movieDetailsQuery) {
   try {
     const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY || "";
     const apiUrl = process.env.NEXT_PUBLIC_OMDB_API_URL || "http://www.omdbapi.com";
-    const response = await axios.get(`${apiUrl}/?apikey=${apiKey}&`, {
+    const response = await axios.get(`${apiUrl}/?apikey=${apiKey}`, {
       params: movieDetailsQuery,
       headers: {
         "Content-Type": "application/json",
